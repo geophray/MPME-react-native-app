4371bcd721202ccc627a8863497dccc54ab19198
Jeff Maughan <jeff.maughan@intimetec.com>
2020-11-26 10:40:15 -0700

    Add react-navigation npm module

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello, world!</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    backgroundColor: 'lavender',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
   },
 });
